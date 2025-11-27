2710734184352d32522136e34ce91912602f9d2e
Guts <gustafigsouza@gmail.com>
2025-11-27 12:38:02 +0000

    Melhora design: UI moderna com cores, fontes Segoe UI, ícones, validação robusta e tratamento de erros

--- DevNooTools/Form1.Designer.cs
+++ DevNooTools/Form1.Designer.cs
@@ -7,6 +7,7 @@
         /// </summary>
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Label labelTitle;
         private System.Windows.Forms.Label labelName;
         private System.Windows.Forms.TextBox textName;
         private System.Windows.Forms.Label labelDescription;
@@ -45,6 +46,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.panelTop = new System.Windows.Forms.Panel();
+            this.labelTitle = new System.Windows.Forms.Label();
             this.labelName = new System.Windows.Forms.Label();
             this.textName = new System.Windows.Forms.TextBox();
             this.labelDescription = new System.Windows.Forms.Label();
@@ -59,108 +61,231 @@
             this.btnClear = new System.Windows.Forms.Button();
             this.dataGridViewProducts = new System.Windows.Forms.DataGridView();
             this.bindingSourceProducts = new System.Windows.Forms.BindingSource(this.components);
+            this.panelTop.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProducts)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSourceProducts)).BeginInit();
             this.SuspendLayout();
+
+            // ============================================================
+            // MODERN COLOR PALETTE
+            // ============================================================
+            System.Drawing.Color primaryColor = System.Drawing.Color.FromArgb(41, 128, 185);      // Blue
+            System.Drawing.Color primaryDark = System.Drawing.Color.FromArgb(31, 97, 141);        // Darker Blue
+            System.Drawing.Color successColor = System.Drawing.Color.FromArgb(39, 174, 96);       // Green
+            System.Drawing.Color successDark = System.Drawing.Color.FromArgb(30, 132, 73);        // Darker Green
+            System.Drawing.Color dangerColor = System.Drawing.Color.FromArgb(231, 76, 60);        // Red
+            System.Drawing.Color dangerDark = System.Drawing.Color.FromArgb(176, 58, 46);         // Darker Red
+            System.Drawing.Color warningColor = System.Drawing.Color.FromArgb(243, 156, 18);      // Orange
+            System.Drawing.Color warningDark = System.Drawing.Color.FromArgb(185, 119, 14);       // Darker Orange
+            System.Drawing.Color bgLight = System.Drawing.Color.FromArgb(248, 249, 250);          // Light Gray BG
+            System.Drawing.Color bgDark = System.Drawing.Color.FromArgb(52, 73, 94);              // Dark Header
+            System.Drawing.Color textLight = System.Drawing.Color.White;
+            System.Drawing.Color textDark = System.Drawing.Color.FromArgb(44, 62, 80);
+            System.Drawing.Color borderColor = System.Drawing.Color.FromArgb(189, 195, 199);
+
+            // ============================================================
+            // FONTS
+            // ============================================================
+            System.Drawing.Font fontTitle = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            System.Drawing.Font fontLabel = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            System.Drawing.Font fontInput = new System.Drawing.Font("Segoe UI", 10F);
+            System.Drawing.Font fontButton = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            System.Drawing.Font fontGrid = new System.Drawing.Font("Segoe UI", 9F);
+
             // 
             // panelTop
             // 
             this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelTop.Height = 140;
-            this.panelTop.Padding = new System.Windows.Forms.Padding(8);
-            this.panelTop.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panelTop.Height = 180;
+            this.panelTop.Padding = new System.Windows.Forms.Padding(20);
+            this.panelTop.BackColor = bgLight;
+
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Location = new System.Drawing.Point(20, 15);
+            this.labelTitle.Text = "📦 Gerenciador de Produtos";
+            this.labelTitle.Font = fontTitle;
+            this.labelTitle.ForeColor = textDark;
+
             // 
             // labelName
             // 
             this.labelName.AutoSize = true;
-            this.labelName.Location = new System.Drawing.Point(12, 12);
-            this.labelName.Text = "Nome";
+            this.labelName.Location = new System.Drawing.Point(20, 55);
+            this.labelName.Text = "NOME *";
+            this.labelName.Font = fontLabel;
+            this.labelName.ForeColor = textDark;
+
             // 
             // textName
             // 
-            this.textName.Location = new System.Drawing.Point(12, 30);
-            this.textName.Width = 260;
+            this.textName.Location = new System.Drawing.Point(20, 75);
+            this.textName.Size = new System.Drawing.Size(220, 28);
+            this.textName.Font = fontInput;
+            this.textName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+
             // 
             // labelDescription
             // 
             this.labelDescription.AutoSize = true;
-            this.labelDescription.Location = new System.Drawing.Point(290, 12);
-            this.labelDescription.Text = "Descrição";
+            this.labelDescription.Location = new System.Drawing.Point(260, 55);
+            this.labelDescription.Text = "DESCRIÇÃO";
+            this.labelDescription.Font = fontLabel;
+            this.labelDescription.ForeColor = textDark;
+
             // 
             // textDescription
             // 
-            this.textDescription.Location = new System.Drawing.Point(290, 30);
-            this.textDescription.Width = 360;
+            this.textDescription.Location = new System.Drawing.Point(260, 75);
+            this.textDescription.Size = new System.Drawing.Size(320, 28);
+            this.textDescription.Font = fontInput;
+            this.textDescription.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+
             // 
             // labelPrice
             // 
             this.labelPrice.AutoSize = true;
-            this.labelPrice.Location = new System.Drawing.Point(12, 64);
-            this.labelPrice.Text = "Preço";
+            this.labelPrice.Location = new System.Drawing.Point(600, 55);
+            this.labelPrice.Text = "PREÇO (R$)";
+            this.labelPrice.Font = fontLabel;
+            this.labelPrice.ForeColor = textDark;
+
             // 
             // textPrice
             // 
-            this.textPrice.Location = new System.Drawing.Point(12, 82);
-            this.textPrice.Width = 120;
+            this.textPrice.Location = new System.Drawing.Point(600, 75);
+            this.textPrice.Size = new System.Drawing.Size(100, 28);
+            this.textPrice.Font = fontInput;
+            this.textPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+
             // 
             // labelQuantity
             // 
             this.labelQuantity.AutoSize = true;
-            this.labelQuantity.Location = new System.Drawing.Point(150, 64);
-            this.labelQuantity.Text = "Quantidade";
+            this.labelQuantity.Location = new System.Drawing.Point(720, 55);
+            this.labelQuantity.Text = "QTD";
+            this.labelQuantity.Font = fontLabel;
+            this.labelQuantity.ForeColor = textDark;
+
             // 
             // textQuantity
             // 
-            this.textQuantity.Location = new System.Drawing.Point(150, 82);
-            this.textQuantity.Width = 120;
+            this.textQuantity.Location = new System.Drawing.Point(720, 75);
+            this.textQuantity.Size = new System.Drawing.Size(70, 28);
+            this.textQuantity.Font = fontInput;
+            this.textQuantity.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textQuantity.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(290, 76);
-            this.btnAdd.Text = "Adicionar";
+            this.btnAdd.Location = new System.Drawing.Point(20, 120);
+            this.btnAdd.Size = new System.Drawing.Size(110, 38);
+            this.btnAdd.Text = "➕ Adicionar";
+            this.btnAdd.Font = fontButton;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.BackColor = successColor;
+            this.btnAdd.ForeColor = textLight;
+            this.btnAdd.FlatAppearance.BorderSize = 0;
+            this.btnAdd.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(380, 76);
-            this.btnSave.Text = "Salvar";
+            this.btnSave.Location = new System.Drawing.Point(140, 120);
+            this.btnSave.Size = new System.Drawing.Size(100, 38);
+            this.btnSave.Text = "💾 Salvar";
+            this.btnSave.Font = fontButton;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.BackColor = primaryColor;
+            this.btnSave.ForeColor = textLight;
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(470, 76);
-            this.btnDelete.Text = "Excluir";
+            this.btnDelete.Location = new System.Drawing.Point(250, 120);
+            this.btnDelete.Size = new System.Drawing.Size(100, 38);
+            this.btnDelete.Text = "🗑️ Excluir";
+            this.btnDelete.Font = fontButton;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.BackColor = dangerColor;
+            this.btnDelete.ForeColor = textLight;
+            this.btnDelete.FlatAppearance.BorderSize = 0;
+            this.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(560, 76);
-            this.btnClear.Text = "Limpar";
+            this.btnClear.Location = new System.Drawing.Point(360, 120);
+            this.btnClear.Size = new System.Drawing.Size(100, 38);
+            this.btnClear.Text = "🔄 Limpar";
+            this.btnClear.Font = fontButton;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.BackColor = warningColor;
+            this.btnClear.ForeColor = textLight;
+            this.btnClear.FlatAppearance.BorderSize = 0;
+            this.btnClear.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+
             // 
             // dataGridViewProducts
             // 
             this.dataGridViewProducts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
             | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridViewProducts.Location = new System.Drawing.Point(12, 150);
-            this.dataGridViewProducts.Size = new System.Drawing.Size(760, 290);
+            this.dataGridViewProducts.Location = new System.Drawing.Point(20, 195);
+            this.dataGridViewProducts.Size = new System.Drawing.Size(860, 320);
             this.dataGridViewProducts.AutoGenerateColumns = true;
             this.dataGridViewProducts.ReadOnly = true;
             this.dataGridViewProducts.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridViewProducts.MultiSelect = false;
             this.dataGridViewProducts.DataSource = this.bindingSourceProducts;
+            this.dataGridViewProducts.AllowUserToAddRows = false;
+            this.dataGridViewProducts.AllowUserToDeleteRows = false;
+            this.dataGridViewProducts.AllowUserToResizeRows = false;
+            this.dataGridViewProducts.RowHeadersVisible = false;
+            this.dataGridViewProducts.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewProducts.Font = fontGrid;
+            this.dataGridViewProducts.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridViewProducts.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridViewProducts.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dataGridViewProducts.GridColor = borderColor;
+            this.dataGridViewProducts.DefaultCellStyle.SelectionBackColor = primaryColor;
+            this.dataGridViewProducts.DefaultCellStyle.SelectionForeColor = textLight;
+            this.dataGridViewProducts.DefaultCellStyle.Padding = new System.Windows.Forms.Padding(5);
+            this.dataGridViewProducts.ColumnHeadersDefaultCellStyle.BackColor = bgDark;
+            this.dataGridViewProducts.ColumnHeadersDefaultCellStyle.ForeColor = textLight;
+            this.dataGridViewProducts.ColumnHeadersDefaultCellStyle.Font = fontLabel;
+            this.dataGridViewProducts.ColumnHeadersDefaultCellStyle.Padding = new System.Windows.Forms.Padding(8);
+            this.dataGridViewProducts.EnableHeadersVisualStyles = false;
+            this.dataGridViewProducts.ColumnHeadersHeight = 40;
+            this.dataGridViewProducts.RowTemplate.Height = 35;
+            this.dataGridViewProducts.AlternatingRowsDefaultCellStyle.BackColor = System.Drawing.Color.FromArgb(245, 247, 250);
             this.dataGridViewProducts.SelectionChanged += new System.EventHandler(this.dataGridViewProducts_SelectionChanged);
+
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 540);
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.BackColor = bgLight;
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.Controls.Add(this.dataGridViewProducts);
             this.Controls.Add(this.panelTop);
             this.Text = "DevNooTools - Gerenciador de Produtos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 
             // Add controls to panelTop
+            this.panelTop.Controls.Add(this.labelTitle);
             this.panelTop.Controls.Add(this.labelName);
             this.panelTop.Controls.Add(this.textName);
             this.panelTop.Controls.Add(this.labelDescription);
@@ -174,6 +299,8 @@
             this.panelTop.Controls.Add(this.btnDelete);
             this.panelTop.Controls.Add(this.btnClear);
 
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProducts)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSourceProducts)).EndInit();
             this.ResumeLayout(false);
--- DevNooTools/Form1.cs
+++ DevNooTools/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,67 +24,143 @@ namespace DevNooTools
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            repository = new ProductRepository();
-            var list = repository.LoadAll();
-            products = new BindingList<Product>(list);
-            bindingSourceProducts.DataSource = products;
+            try
+            {
+                repository = new ProductRepository();
+                var list = repository.LoadAll();
+                products = new BindingList<Product>(list);
+                bindingSourceProducts.DataSource = products;
+                
+                // Clear selection initially
+                dataGridViewProducts.ClearSelection();
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao carregar produtos: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                products = new BindingList<Product>();
+                bindingSourceProducts.DataSource = products;
+            }
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
+            // Validate name (required)
             if (string.IsNullOrWhiteSpace(textName.Text))
             {
                 MessageBox.Show("Nome é obrigatório.", "Validação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textName.Focus();
                 return;
             }
 
-            if (!decimal.TryParse(textPrice.Text, out var price)) price = 0m;
-            if (!int.TryParse(textQuantity.Text, out var qty)) qty = 0;
+            // Parse price with culture support
+            decimal price = ParseDecimal(textPrice.Text);
+            int qty = ParseInt(textQuantity.Text);
+
+            // Validate non-negative values
+            if (price < 0)
+            {
+                MessageBox.Show("Preço não pode ser negativo.", "Validação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textPrice.Focus();
+                return;
+            }
+
+            if (qty < 0)
+            {
+                MessageBox.Show("Quantidade não pode ser negativa.", "Validação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textQuantity.Focus();
+                return;
+            }
 
             var p = new Product
             {
                 Name = textName.Text.Trim(),
-                Description = textDescription.Text.Trim(),
+                Description = textDescription.Text?.Trim() ?? string.Empty,
                 Price = price,
                 Quantity = qty
             };
 
             products.Add(p);
-            repository.SaveAll(new List<Product>(products));
+            SaveProducts();
             ClearFields();
+            
+            // Select the new item
+            dataGridViewProducts.ClearSelection();
+            int lastRowIndex = dataGridViewProducts.Rows.Count - 1;
+            if (lastRowIndex >= 0)
+            {
+                dataGridViewProducts.Rows[lastRowIndex].Selected = true;
+                dataGridViewProducts.FirstDisplayedScrollingRowIndex = lastRowIndex;
+            }
+
+            MessageBox.Show("Produto adicionado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            if (bindingSourceProducts.Current is Product cur)
-            {
-                cur.Name = textName.Text.Trim();
-                cur.Description = textDescription.Text.Trim();
-                if (!decimal.TryParse(textPrice.Text, out var price)) price = 0m;
-                if (!int.TryParse(textQuantity.Text, out var qty)) qty = 0;
-                cur.Price = price;
-                cur.Quantity = qty;
-                bindingSourceProducts.ResetBindings(false);
-                repository.SaveAll(new List<Product>(products));
-                MessageBox.Show("Produto atualizado.", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else
+            if (!(bindingSourceProducts.Current is Product cur))
             {
                 MessageBox.Show("Nenhum produto selecionado.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
             }
+
+            // Validate name (required)
+            if (string.IsNullOrWhiteSpace(textName.Text))
+            {
+                MessageBox.Show("Nome é obrigatório.", "Validação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textName.Focus();
+                return;
+            }
+
+            decimal price = ParseDecimal(textPrice.Text);
+            int qty = ParseInt(textQuantity.Text);
+
+            // Validate non-negative values
+            if (price < 0)
+            {
+                MessageBox.Show("Preço não pode ser negativo.", "Validação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textPrice.Focus();
+                return;
+            }
+
+            if (qty < 0)
+            {
+                MessageBox.Show("Quantidade não pode ser negativa.", "Validação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textQuantity.Focus();
+                return;
+            }
+
+            cur.Name = textName.Text.Trim();
+            cur.Description = textDescription.Text?.Trim() ?? string.Empty;
+            cur.Price = price;
+            cur.Quantity = qty;
+
+            bindingSourceProducts.ResetBindings(false);
+            SaveProducts();
+            MessageBox.Show("Produto atualizado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            if (bindingSourceProducts.Current is Product cur)
+            if (!(bindingSourceProducts.Current is Product cur))
             {
-                var res = MessageBox.Show($"Excluir '{cur.Name}'?", "Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                if (res == DialogResult.Yes)
-                {
-                    products.Remove(cur);
-                    repository.SaveAll(new List<Product>(products));
-                    ClearFields();
-                }
+                MessageBox.Show("Nenhum produto selecionado.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var res = MessageBox.Show(
+                $"Tem certeza que deseja excluir o produto '{cur.Name}'?\n\nEsta ação não pode ser desfeita.",
+                "Confirmar Exclusão",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button2);
+
+            if (res == DialogResult.Yes)
+            {
+                products.Remove(cur);
+                SaveProducts();
+                ClearFields();
+                MessageBox.Show("Produto excluído com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
@@ -105,15 +182,53 @@ namespace DevNooTools
         {
             if (bindingSourceProducts.Current is Product cur)
             {
-                textName.Text = cur.Name;
-                textDescription.Text = cur.Description;
-                textPrice.Text = cur.Price.ToString();
+                textName.Text = cur.Name ?? string.Empty;
+                textDescription.Text = cur.Description ?? string.Empty;
+                textPrice.Text = cur.Price.ToString("F2", CultureInfo.CurrentCulture);
                 textQuantity.Text = cur.Quantity.ToString();
             }
             else
             {
                 ClearFields();
             }
+        }
+
+        private void SaveProducts()
+        {
+            try
+            {
+                repository.SaveAll(new List<Product>(products));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao salvar: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private decimal ParseDecimal(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return 0m;
+
+            // Try current culture first, then invariant
+            if (decimal.TryParse(text, NumberStyles.Any, CultureInfo.CurrentCulture, out var val))
+                return val;
+
+            if (decimal.TryParse(text, NumberStyles.Any, CultureInfo.InvariantCulture, out val))
+                return val;
+
+            return 0m;
+        }
+
+        private int ParseInt(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return 0;
+
+            if (int.TryParse(text, out var val))
+                return val;
+
+            return 0;
         }
     }
 }
